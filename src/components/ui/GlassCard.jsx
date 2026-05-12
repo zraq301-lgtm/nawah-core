c5c0f92875624d610aa46af98af453e93df80d2d
zraq301-lgtm <zraq301@gmail.com>
2026-05-13 02:27:23 +0300

    Delete src/components directory

--- src/components/ui/GlassCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const GlassCard = ({ children, className = "" }) => (
-  <div className={`bg-white/20 backdrop-blur-md border border-white/30 rounded-3xl p-6 shadow-xl ${className}`}>
-    {children}
-  </div>
-);
-
-export default GlassCard;
